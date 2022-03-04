46a15d134d2466e1c800c09e9a84163f6ca81c52
Raphael Thomaz <raphaeloliveira@pecege.com>
2022-03-04 12:17:31 -0300

    feat: add functions to transactions type button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer } from './styles';
+import { api } from '../../services/api';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,6 +15,19 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [type, setType] = useState('deposit');
+  const [category, setCategory] = useState('');
+
+  const handleCreateNewTransaction = (event: FormEvent) => {
+    event.preventDefault();
+
+    const data = { title, value, category, type };
+
+    api.post('/transactions', data);
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -28,26 +43,51 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
 
-        <input type="text" placeholder="Título" />
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Título"
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input
+          type="number"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+          placeholder="Valor"
+        />
 
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
-        <input type="text" placeholder="Categoria" />
+        <input
+          type="text"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+          placeholder="Categoria"
+        />
 
         <button type="submit">Cadastrar</button>
       </Container>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2{
@@ -50,11 +50,27 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
 
-  button{
+`
+
+interface RadioBoxProps{
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = { 
+  red: '#FA8072',
+  green: '#33CC95'
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: transparent;
+
+    background: ${(props)=> props.isActive ? 
+    transparentize(0.9,colors[props.activeColor]): 
+    'transparent'};
+
     display: flex;
     align-items: center;
     justify-content: center;
@@ -75,5 +91,4 @@ export const TransactionTypeContainer = styled.div`
     &:hover{
       border-color: ${darken(0.2, '#d7d7d7')};
     }
-  }
 `
